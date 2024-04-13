d3c86588285098b2aa7eab9bcb556e7da9b53c53
Anantha vaishnav A <152788361+ananth-vaishnav@users.noreply.github.com>
2024-04-13 19:16:14 +0530

    main.js

--- main.js
+++ main.js
@@ -7,24 +7,15 @@ const aliceTiming = {
   duration: 2000,
   iterations: 1,
   fill: 'forwards'
-}
-
-const alice1 = document.querySelector("#alice1");
-const alice2 = document.querySelector("#alice2");
-const alice3 = document.querySelector("#alice3");
-const aliceTumbling = [
-  { transform: "rotate(0) scale(1)" },
-  { transform: "rotate(360deg) scale(0)" },
-];
-
-const aliceTiming = {
-  duration: 2000,
-  iterations: 1,
-  fill: "forwards",
 };
 
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
-
-alice1.animate(aliceTumbling, aliceTiming);
+const animation1 = alice1.animate(aliceTumbling, aliceTiming);
+animation1.onfinish = () => {
+  const animation2 = alice2.animate(aliceTumbling, aliceTiming);
+  animation2.onfinish = () => {
+    alice3.animate(aliceTumbling, aliceTiming);
+  };
+};
